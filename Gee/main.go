19f5ebf6ec0e427a728b5843149e07b1a049a63f
Tom5977 <1441541389@qq.com>
2023-11-14 17:58:38 +0800

    gee.go

--- Gee/main.go
+++ Gee/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"Gee/gee"
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	r := gee.New()
+	r.Get("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf()
+	})
 }
