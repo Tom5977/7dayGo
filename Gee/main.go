20edafb3cb3fc5d79938e8304df8e02a39643456
Tom5977 <1441541389@qq.com>
2023-11-17 17:16:07 +0800

    5/7

--- Gee/main.go
+++ Gee/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"Gee/gee"
+	"log"
 	"net/http"
+	"time"
 )
+
+func onlyForV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		t := time.Now()
+		// c.Fail(500, "Internal Server Error")
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
 
 func main() {
 	r := gee.New()
+	r.Use(gee.Logger())
 	r.GET("/index", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
@@ -22,6 +33,7 @@ func main() {
 		})
 	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2())
 	{
 		v2.GET("/hello/:name", func(c *gee.Context) {
 			// expect /hello/geektutu
