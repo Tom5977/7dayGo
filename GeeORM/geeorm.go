70e6fd650a11604c6e461fc68657f2c0f3a71d11
Tom5977 <1441541389@qq.com>
2023-12-08 14:29:59 +0800

    6/7GeeORM

--- GeeORM/geeorm.go
+++ GeeORM/geeorm.go
@@ -12,6 +12,8 @@ type Engine struct {
 	dialect dialect.Dialect
 }
 
+type TxFunc func(*session.Session) (interface{}, error)
+
 func NewEngine(driver, source string) (e *Engine, err error) {
 	db, err := sql.Open(driver, source)
 	if err != nil {
@@ -30,6 +32,24 @@ func NewEngine(driver, source string) (e *Engine, err error) {
 	e = &Engine{db: db, dialect: dial}
 	log.Info("Connect database success")
 	return
+}
+
+func (engine *Engine) Transaction(f TxFunc) (result interface{}, err error) {
+	s := engine.NewSession()
+	if err := s.Begin(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = s.Rollback()
+			panic(p)
+		} else if err != nil {
+			_ = s.Rollback()
+		} else {
+			err = s.Commit()
+		}
+	}()
+	return f(s)
 }
 
 func (engine *Engine) Close() {
