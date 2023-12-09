dc5211d483c9a8bbd34a9cac6c1a215b156f5223
Tom5977 <1441541389@qq.com>
2023-12-09 14:46:12 +0800

    7/7GeeORM Done!

--- GeeORM/geerom_test.go
+++ GeeORM/geerom_test.go
@@ -3,6 +3,7 @@ package geeorm
 import (
 	"errors"
 	"geeorm/session"
+	"reflect"
 	"testing"
 )
 
@@ -59,4 +60,20 @@ func TestEngine_Transaction(t *testing.T) {
 	t.Run("commit", func(t *testing.T) {
 		transactionCommit(t)
 	})
+}
+
+func TestEngine_Migrate(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text PRIMARY KEY, XXX integer);").Exec()
+	_, _ = s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam").Exec()
+	engine.Migrate(&User{})
+
+	rows, _ := s.Raw("SELECT * FROM User").QueryRows()
+	columns, _ := rows.Columns()
+	if !reflect.DeepEqual(columns, []string{"Name", "Age"}) {
+		t.Fatal("Failed to migrate table User, got columns", columns)
+	}
 }
